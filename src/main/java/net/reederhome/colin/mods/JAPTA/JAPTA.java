dc0977e51764da6d77de8967cea0880749241073
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-07-26 14:04:39 -0600

    Fix Cake Converter recipe

--- src/main/java/net/reederhome/colin/mods/JAPTA/JAPTA.java
+++ src/main/java/net/reederhome/colin/mods/JAPTA/JAPTA.java
@@ -113,7 +113,7 @@ public class JAPTA {
 		addRecipe(new ShapedOreRecipe(elevatorTop, "grg", "rer", "rsr", 'r', "dustRedstone", 'e', Items.ender_pearl, 's', elevatorShaft, 'g', "nuggetGold"));
 		addRecipe(new ShapedOreRecipe(timeMachine, "oro", "rcr", "oro", 'o', Blocks.obsidian, 'r', "blockRedstone", 'c', Items.clock));
 		addRecipe(new ShapedOreRecipe(chestCharger, "rRr", "gcg", "oRo", 'r', "dustRedstone", 'R', "blockRedstone", 'g', "nuggetGold", 'c', Blocks.chest, 'o', Blocks.obsidian));
-		addRecipe(new ShapedOreRecipe(cakeConverter, "frf", "rgr", "frf", 'f', Blocks.cake, 'r', "dustRedstone", 'g', "ingotGold"));
+		addRecipe(new ShapedOreRecipe(cakeConverter, "frf", "rgr", "frf", 'f', Items.cake, 'r', "dustRedstone", 'g', "ingotGold"));
 		addRecipe(new ShapedOreRecipe(fluxBlaster, "dbd", "bgb", "dbd", 'd', "dustRedstone", 'b', "blockRedstone", 'g', "ingotGold"));
 		addRecipe(new ShapedOreRecipe(bonemealApplicator, "gbg", "brb", "gbg", 'g', "nuggetGold", 'b', new ItemStack(Items.dye, 1, 15), 'r', "blockRedstone"));
 		addRecipe(new ShapedOreRecipe(new ItemStack(mover, 4), "rgr", "gpg", "rgr", 'r', "dustRedstone", 'g', "nuggetGold", 'p', Blocks.piston));
