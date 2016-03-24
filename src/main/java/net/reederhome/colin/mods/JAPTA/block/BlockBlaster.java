585471c92e0a8120982543b1d80a1d25881d3d0e
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-03-24 17:18:07 -0600

    Fix Item Blaster

--- src/main/java/net/reederhome/colin/mods/JAPTA/block/BlockBlaster.java
+++ src/main/java/net/reederhome/colin/mods/JAPTA/block/BlockBlaster.java
@@ -6,14 +6,19 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.reederhome.colin.mods.JAPTA.IDiagnosable;
 import net.reederhome.colin.mods.JAPTA.JAPTA;
 
-public abstract class BlockBlaster extends BlockContainer {
+public abstract class BlockBlaster extends BlockContainer implements IDiagnosable {
 
     public static final PropertyEnum<EnumFacing> FACING = PropertyEnum.create("facing", EnumFacing.class);
     public static int RANGE = 8;
@@ -60,4 +65,16 @@ public abstract class BlockBlaster extends BlockContainer {
         return getDefaultState().withProperty(FACING, ts);
     }
 
+    @Override
+    public boolean addInformation(ICommandSender sender, IBlockAccess world, BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+        EnumFacing side = state.getValue(FACING);
+        sender.addChatMessage(new ChatComponentTranslation("tile.blaster.diagnostic.top", side.getName()));
+        for(int i = 1; i <= RANGE; i++) {
+            BlockPos cp = pos.offset(side, i);
+            IBlockState cs = world.getBlockState(cp);
+            sender.addChatMessage(new ChatComponentText(cs.getBlock().getUnlocalizedName()));
+        }
+        return true;
+    }
 }
