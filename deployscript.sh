7deb0a4fed8808a2274c8524363003efaee7d7d4
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-10-10 13:41:14 -0600

    OH.

--- deployscript.sh
+++ deployscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./gradlew curseforge
-curl -i -H "X-API-Key: $MODSIO_API_KEY" -F body="{\"version\":{\"name\":\"$(basename $THE_JAR | sed "s/JAPTA-1.10.0-\(.*\).jar/\1/g")\",\"minecraft\":\"1.10.0\"},\"filename\": \"$(basename $THE_JAR)\"}" -F file=@${THE_JAR}  https://mods.io/mods/1214/versions/create.json
+curl -i -H "X-API-Key: $MODSIO_API_KEY" -F body="{\"version\":{\"name\":\"$(basename $THE_JAR | sed "s/JAPTA-1.10.2-\(.*\).jar/\1/g")\",\"minecraft\":\"1.10.2\"},\"filename\": \"$(basename $THE_JAR)\"}" -F file=@${THE_JAR}  https://mods.io/mods/1214/versions/create.json
